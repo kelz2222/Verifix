c593d9f44650db165ce62ed9d704c91c46d2500d
kelz2222 <kelechicchukwu34@gmail.com>
2026-08-19 20:56:37 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Browse from './pages/Browse';
+import ArtisanProfile from './pages/ArtisanProfile';
 
 export default function App() {
   const location = useLocation();
@@ -10,6 +12,8 @@ export default function App() {
     <div className="max-w-md mx-auto bg-[#F7F9FA] min-h-screen relative">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path="/artisan/:id" element={<ArtisanProfile />} />
       </Routes>
       {!hideNavbar && <Navbar />}
     </div>
